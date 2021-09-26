f0783b1597fcbb50920821b147aee9d645cd665d
umit ceylan <byildiz2019@gmail.com>
2021-09-26 23:12:54 +0200

    w

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-public class hello {
+public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello fromf fgfg Java");
+        System.out.println("Hello from Java");
     }
 }
