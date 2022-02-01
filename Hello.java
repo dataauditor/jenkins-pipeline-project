eb7156a33ba552bb6b7ac1d8124b90adf7cf970e
Umit CEYLAN <byildiz2019@gmail.com>
2022-02-01 20:52:03 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Ahmet from umit");
+        System.out.println("Hello Ahmet from Umit");
     }
 }
